8197f921c65a9d9afb1bbc8ba45eb7553be46dd3
Wapiti08 <tzrzhuoran@gmail.com>
2024-09-29 21:32:09 +0100

    finish code to simulate dev and office device

--- daily/devm.py
+++ daily/devm.py
@@ -12,5 +12,83 @@ simulated behaviours:
     - download
 
  '''
+from statemachine import (copy, dev, web_visit, api_sev, update_call, rdp, download)
+import os
+from core import config
+import random
+from pathlib import Path
+import platform
+from utils import util
+
+logger = util.create_logger(Path.cwd().parent.joinpath("logs", Path(__file__).name))
+
+
+class DevVM:
+    def __init__(self, remote_host, local_file, remote_path):
+        self.hostname = remote_host
+        self.host_username = os.environ.get("HOST_USERNAME")
+        self.host_password = os.environ.get("HOST_PASSWORD")
+        # set default ssh port as 22
+        self.ssh_port = 22
+
+        self.remote_path = remote_path
+        self.local_path = local_file
+
+
+    def _API_CALL(self,):
+        api_sev.chat_with_gpt()
+
+
+    def _COPY(self,):
+        ''' simulate scp for copy between two hosts
+        
+        '''
+        copy.scp_copy_file(self.hostname, self.ssh_port, \
+                           self.host_username, self.host_password, self.local_path, \
+                            self.remote_path)
+
+    # def _RDP(self,):
+    #     ''' similar to scp
+        
+    #     '''
+    #     pass
+
+
+    def _DOWNLOAD(self,):
+        
+        download.clone_repository(random.choice(config.git_reps_list))
+
+    def _DEV(self,):
+        dev.create_code_files()
+        project_path = Path.cwd().joinpath("example_project")
+        dev.run_tests()
+        deployment_directory = Path.cwd().joinpath("deployed_example_project")
+        dev.deploy_application(project_path, deployment_directory)
+
+    
+    def _UPDATE_CALL(self,):
+        os_type = platform.system()
+        if os_type.startswith("Windows"):
+            update_call.update_windows()
+        
+        elif os_type.startswith("Linux"):
+            update_call.update_linux_debian()
+        
+        elif os_type.startswith("Darwin"):
+            update_call.update_mac_brew()
+        else:
+            logger.info("The update call is not able on {os_type}")
+
+
+    def _WebVisit(self,):
+        driver = web_visit.load_driver()
+        url_list = config.web_sites
+        url = random.choice(url_list)
+        web_visit.website_access(driver, url)
+    
+
+
+
+
 
 
--- daily/officem.py
+++ daily/officem.py
@@ -15,7 +15,8 @@ simulated behaviours:
 from statemachine import api_sev, logins, office, web_visit
 from utils import sche
 import os
-
+from core import config
+import random
 
 class OfficeVM:
     def __init__(self, login_url):
@@ -24,23 +25,55 @@ class OfficeVM:
         self.login_url = login_url
 
     def _API_CALL(self,):
+        ''' simulate chatgpt call
+        
+        '''
         api_sev.chat_with_gpt()
 
     
     def _LOGIN(self,):
+        ''' simulate website login behaviour
+        
+        '''
         logins.login_simu(
-            self.login_url, {
-                            "username": self.web_username ,
-                            "password": self.web_password
-                            }
+            self.login_url, 
+            {
+            "username": self.web_username ,
+            "password": self.web_password
+                }
         )
 
     
     def _OFFICE(self,):
+        ''' simulate office work
         
+        '''
+        # document manipulations
+        office.create_doc()
+        office.modify_doc()
+        office.delete_doc()
+
+        # powerpoint manipulation
+        office.create_ppt()
+        office.modify_ppt()
+        office.delete_ppt()
+
+        # excel manipulation
+        office.create_xls()
+        office.modify_xls()
+        office.delete_xls()
+
+        # click event
+        office.automate_gui()
     
     def _WebVisit(self,):
-    
+        ''' simulate web visit
+        
+        '''
+        driver = web_visit.load_driver()
+        url_list = config.web_sites
+        url = random.choice(url_list)
+        web_visit.website_access(driver, url)
     
 
 
